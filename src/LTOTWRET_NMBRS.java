faf1eed4086e2cc40c4749a869d53543934c04a4
BigRockerEnding <bigrockerending@live.com>
2015-12-15 17:00:13 -0800

    FART
    M3M3

---
+++ src/LTOTWRET_NMBRS.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class LTOTWRET_NMBRS {
+	public static void main(String[] args){
+int rndm = new Random().nextInt(49)+1;
+int random = new Random().nextInt(49)+1;
+int randm = new Random().nextInt(49)+1;
+int r4nd0m = new Random().nextInt(49)+1;
+int rndom = new Random().nextInt(49)+1;
+int radom = new Random().nextInt(49)+1;
+JOptionPane.showMessageDialog(null, rndm);
+JOptionPane.showMessageDialog(null, random);
+JOptionPane.showMessageDialog(null, randm);
+JOptionPane.showMessageDialog(null, r4nd0m);
+JOptionPane.showMessageDialog(null, rndom);
+JOptionPane.showMessageDialog(null, radom);
+}
+}
