259165a91a0cd65f9764b39478a565fc8e86888f
Lucas Baizer <no>
2016-02-09 16:58:52 -0800

    vhv
    exactly what ash ketchum would say

---
+++ src/memememememmememe.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class memememememmememe {
+public static void main(String[] args) {
+String height = JOptionPane.showInputDialog("how tall are you in inches?");
+int h = Integer.parseInt(height);
+
+if (h>=48){
+	JOptionPane.showMessageDialog(null, "go ahead");
+}
+else {
+	JOptionPane.showMessageDialog(null, "you cant go on this ride, go one the crazy sub marine");
+}
+}
+}
